e6de8e4554b0dbc5b346b587eff6b72bc40acd0a
diegocampos1 <diegodiko05@gmail.com>
2021-10-18 23:44:11 -0300

    Feature: Dark Mode

--- src/App.js
+++ src/App.js
@@ -1,12 +1,28 @@
 import React from 'react';
+import { ThemeProvider } from 'styled-components';
+import useDarkMode from './hooks/useDarkMode';
+import GlobalStyle from './globalStyles/Global';
+import { lightTheme, darkTheme } from './globalStyles/Themes';
+import Toggle from './components/Toggle';
 import './App.css';
 
-function App() {
+const App = () => {
+  const [theme, themeToggler, mountedComponent] = useDarkMode();
+
+  const themeMode = theme === 'light' ? lightTheme : darkTheme;
+
+  if (!mountedComponent) return <div />;
+
   return (
-    <div>
-      Hello App;
-    </div>
+    <ThemeProvider theme={ themeMode }>
+      <>
+        <GlobalStyle />
+        <Toggle theme={ theme } toggleTheme={ themeToggler } />
+        <h1>Teste</h1>
+      </>
+    </ThemeProvider>
+
   );
-}
+};
 
 export default App;
---
+++ src/hooks/useDarkMode.js
@@ -0,0 +1,29 @@
+import { useEffect, useState } from 'react';
+
+const useDarkMode = () => {
+  const [theme, setTheme] = useState('light');
+  const [mountedComponent, setMountedComponent] = useState(false);
+  const setMode = (mode) => {
+    window.localStorage.setItem('theme', mode);
+    setTheme(mode);
+  };
+
+  const themeToggler = () => (
+    theme === 'light' ? setMode('dark') : setMode('light')
+  );
+
+  useEffect(() => {
+    const localTheme = window.localStorage.getItem('theme');
+    if (localTheme) {
+      setTheme(localTheme);
+    } else {
+      setMode('light');
+    }
+
+    setMountedComponent(true);
+  }, []);
+
+  return [theme, themeToggler, mountedComponent];
+};
+
+export default useDarkMode;
