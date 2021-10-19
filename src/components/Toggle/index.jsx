f337b5bfa4452080d0d337bd1c9bb8bd91b76f0b
diegocampos1 <diegodiko05@gmail.com>
2021-10-19 16:02:38 -0300

    Refactor: Adjust design patern

--- src/components/Toggle/index.jsx
+++ src/components/Toggle/index.jsx
@@ -1,34 +1,6 @@
 import React from 'react';
 import { func, string } from 'prop-types';
-import styled from 'styled-components';
-
-const Button = styled.button`
-  
-  border: 2px solid ${({ theme }) => theme.toggleBorder};
-  border-radius: 30px;
-  cursor: pointer;
-  font-size: 0.5rem;
-  justify-content: space-between;
-  margin: 0 auto;
-  overflow: hidden;
-  padding: 0.5rem;
-  position: relative;
-  width: 8rem;
-  height: 4rem;
-  outline: none;
-`;
-
-const Sun = styled.svg`
- height: auto;
- width: 2.5rem;
- transition: all 0.7s linear;
-`;
-
-const Moon = styled.svg`
-  height: auto;
-  width: 2.5rem;
-  transition: all 0.7s linear;
-`;
+import { Button, Sun, Moon } from './styledComponents';
 
 const Toggle = ({ theme, toggleTheme }) => (
   <Button onClick={ toggleTheme }>
