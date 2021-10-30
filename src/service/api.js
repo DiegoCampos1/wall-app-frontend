3f8c4c8536d7638a0ad400ff3904da886580e3e5
diegocampos1 <diegodiko05@gmail.com>
2021-10-30 11:23:29 -0300

    Feature: Fetch posts

---
+++ src/service/api.js
@@ -0,0 +1,21 @@
+import axios from 'axios';
+
+const storedToken = sessionStorage.token;
+
+const baseURL = 'http://localhost:3001/';
+const timeout = 5000;
+const headers = {
+  'Content-Type': 'application/json',
+};
+
+if (storedToken) headers.Authorization = `Token ${storedToken}`;
+
+const instance = axios.create({ baseURL, timeout, headers });
+
+export const getAllPosts = async () => {
+  try {
+    return instance.get('posts');
+  } catch (error) {
+    console.error(error);
+  }
+};
